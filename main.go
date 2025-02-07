8abad596c0009375aa7a0191b1daab8f0ca043cb
alexey.zh <mailbox.stdio@gmail.com>
2025-02-07 19:51:16 +0500

    logger

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"log"
 	"log/slog"
+	"os"
 	"path/filepath"
 
 	"gopgmigrate/internal/migrate"
@@ -12,6 +13,11 @@ import (
 func main() {
 	var err error
 	ctx := context.Background()
+
+	// TODO: these are parameters (envs/cli)
+	// init logger
+	logger := initLogger("console", "debug")
+	slog.SetDefault(logger)
 
 	// TODO: these are parameters (envs/cli)
 	connString := "postgres://postgres:postgres@localhost:5432/bookstore"
@@ -54,4 +60,34 @@ func main() {
 	}
 
 	slog.Info("migrations applied successfully")
+}
+
+func initLogger(enc, lvl string) *slog.Logger {
+	opts := &slog.HandlerOptions{
+		Level: getLoggerLevel(lvl),
+	}
+	var logger *slog.Logger
+	if enc == "console" {
+		logger = slog.New(slog.NewTextHandler(os.Stdout, opts))
+	} else {
+		logger = slog.New(slog.NewJSONHandler(os.Stdout, opts))
+	}
+	return logger
+}
+
+// For mapping config logger to app logger levels
+var loggerLevelMap = map[string]slog.Level{
+	"debug": slog.LevelDebug,
+	"info":  slog.LevelInfo,
+	"warn":  slog.LevelWarn,
+	"error": slog.LevelError,
+}
+
+func getLoggerLevel(lvl string) slog.Level {
+	level, exist := loggerLevelMap[lvl]
+	if !exist {
+		return slog.LevelDebug
+	}
+
+	return level
 }
