3021c7c11d20a3ab89040f7c2f964d76c4657bcb
alexey.zh <mailbox.stdio@gmail.com>
2025-02-09 20:57:26 +0500

    notx - tests

--- internal/migrate/notxwarn_test.go
+++ internal/migrate/notxwarn_test.go
@@ -16,9 +16,10 @@ func TestCheckFileForWarnings(t *testing.T) {
 		{"Detect CREATE INDEX CONCURRENTLY", "CREATE INDEX CONCURRENTLY idx_users ON users (name);", []string{"Warning: Detected CreateIndexConcurrently pattern"}},
 		{"Detect REINDEX", "REINDEX DATABASE mydb;", []string{"Warning: Detected Reindex pattern"}},
 		{"Detect VACUUM", "VACUUM;", []string{"Warning: Detected Vacuum pattern"}},
+		{"Detect VACUUM", "vacuum;", []string{"Warning: Detected Vacuum pattern"}},
 		{"Detect DISCARD ALL", "DISCARD ALL;", []string{"Warning: Detected DiscardAll pattern"}},
 		{"Detect ALTER TYPE ADD VALUE", "ALTER TYPE my_enum ADD VALUE 'new_value';", []string{"Warning: Detected AlterTypeAddValue pattern"}},
-		{"Detect multiple patterns", "VACUUM; CREATE DATABASE newdb; ALTER SYSTEM SET log_statement = 'all';", []string{
+		{"Detect multiple patterns", "vacuum; create database newdb; alter system set log_statement = 'all';", []string{
 			"Warning: Detected Vacuum pattern",
 			"Warning: Detected CreateDatabaseTablespaceSubscription pattern",
 			"Warning: Detected AlterSystem pattern",
