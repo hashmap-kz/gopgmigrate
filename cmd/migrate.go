4eb3cb56817c6633ea405882fed6854bd617a1a6
alexey.zh <mailbox.stdio@gmail.com>
2025-02-13 11:43:29 +0500

    cli - clean code (print mixed mode_

--- cmd/migrate.go
+++ cmd/migrate.go
@@ -81,15 +81,11 @@ func runMigrations(cmd *cobra.Command, args []string) {
 
 func printPending(migrations []migrate.MigrationFile) {
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', tabwriter.TabIndent)
-	printEntries(migrations, w)
-	_ = w.Flush()
-}
-
-func printEntries(migrations []migrate.MigrationFile, w *tabwriter.Writer) {
 	_, _ = fmt.Fprintln(w, "VERSION\tNAME\tPATH")
 	for _, p := range migrations {
 		_, _ = fmt.Fprintf(w, "%d\t%s\t%s\n", p.Vers, p.Base, filepath.ToSlash(p.Path))
 	}
+	_ = w.Flush()
 }
 
 func printPendingGroups(migrations []migrate.MigrationFile) {
@@ -99,9 +95,12 @@ func printPendingGroups(migrations []migrate.MigrationFile) {
 	}
 
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', tabwriter.TabIndent)
+	_, _ = fmt.Fprintln(w, "VERSION\tNAME\tPATH")
 	for batchIdx, batch := range entries {
-		_, _ = fmt.Fprintf(w, "\n-------------- GroupID: %d UseTX: %v --------------\n", batchIdx, batch.UseTX)
-		printEntries(batch.Files, w)
+		_, _ = fmt.Fprintf(w, "GID:%d\tTX:%v\t\n", batchIdx+1, batch.UseTX)
+		for _, p := range batch.Files {
+			_, _ = fmt.Fprintf(w, "%d\t%s\t%s\n", p.Vers, p.Base, filepath.ToSlash(p.Path))
+		}
 	}
 
 	_ = w.Flush()
