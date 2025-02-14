2124f879356018a63e21e1e30b7c37de810ad2ef
alexey.zh <mailbox.stdio@gmail.com>
2025-02-14 11:04:57 +0500

    ch - simplify ch history-table

--- notes/ch/table.sql
+++ notes/ch/table.sql
@@ -1,12 +1,10 @@
-create table if not exists migrate_history
+create table if not exists default.migrate_history
 (
-    id               Int64,
     mh_version       Int64  not null,
     mh_name          String not null,
     mh_hash          String not null,
     mh_applied_by    String                   default currentUser(),
     mh_applied_at    DateTime64(3, 'UTC')     default now64(3),
-    mh_txid          Nullable(String)         default '',
     mh_iter_id       UUID not null,
     mh_version_check UInt64 MATERIALIZED      toUInt64(left(mh_name, 5)),
     constraint       check_filename           check mh_name REGEXP '^(\d{5})-(.*)(?:\.ntx)?\.(do|r)\.sql$',
@@ -32,13 +30,11 @@ where
 ;
 
 select
-    id,
     mh_version,
     mh_name,
     mh_hash,
     mh_applied_by,
     mh_applied_at,
-    mh_txid,
     mh_iter_id
 from migrate_history
 order by mh_version
@@ -50,27 +46,6 @@ where mh_version = 1
 
 select exists (select 1 from migrate_history where mh_version = 1)
 ;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
